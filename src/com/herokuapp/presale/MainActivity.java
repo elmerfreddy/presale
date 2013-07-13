b4b2d98aaf0290b511164d15553a578f08a931a5
Elmer Mendoza <defreddyelmer@gmail.com>
2013-07-13 09:22:37 -0400

    Define main menu

--- src/com/herokuapp/presale/MainActivity.java
+++ src/com/herokuapp/presale/MainActivity.java
@@ -1,10 +1,14 @@
 package com.herokuapp.presale;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +21,28 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onClick(View v) {
+		Intent intent = null;
+		switch(v.getId()) {
+		case: R.id.btnProducts:
+			intent = new Intent(this, ProductsActivity.class);
+			startActivity(intent);
+			break;
+		case: R.id.btnStores:
+			intent = new Intent(this, StoresActivity.class);
+			startActivity(intent);
+			break;
+		case: R.id.btnTransactions:
+			intent = new Intent(this, TransactionsActivity.class);
+			startActivity(intent);
+			break;
+		case: R.id.btnSynchronize:
+			Toast.makeText(this, "Implementar con webservice", Toast.LENGTH_SHORT).show();
+			break;
+		}
 	}
 
 }
